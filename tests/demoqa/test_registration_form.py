380d54eeb5a70e946c8ecf90b0c318c4ec901328
kglazova <kglazova@huntflow.ru>
2024-01-13 10:45:30 +0300

    utils installed

--- tests/demoqa/test_registration_form.py
+++ tests/demoqa/test_registration_form.py
@@ -39,3 +39,4 @@ def test_successful(setup_browser):
         browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
         # browser.element(".table-responsive").should(
         #     have.texts(first_name, last_name, "alex@egorov.com", "Some street 1"))
+        #
